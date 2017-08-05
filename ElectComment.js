10e1b8eeaf7f0e099eb51668db1049b09ee629fa
Crist Ye <iamCristYe@outlook.com>
2017-08-05 11:07:11 -0700

    Create ElectComment.js

---
+++ ElectComment.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         ElectComment
+// @namespace    http://iamCristYe.GitHub.io
+// @version      1.0
+// @description  Automatically elect new comments.
+// @author       Crist
+// @match        https://mp.weixin.qq.com/misc/appmsgcomment?action=list_latest_comment*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    function eventFire(el, etype) {
+        if (el.fireEvent) {
+            el.fireEvent('on' + etype);
+        } else {
+            var evObj = document.createEvent('Events');
+            evObj.initEvent(etype, true, false);
+            el.dispatchEvent(evObj);
+        }
+    }
+
+    function Elect() {
+        switch (document.getElementsByClassName('jsNewElect').length) {
+            case 0:
+                console.log("No comment to be elected.");
+                setInterval(() => {
+                    if (document.getElementById("js_div_newnum").style.display == "block") {
+                        location.reload();
+                    }
+                }, 2000);
+                break;
+            case 1:
+                eventFire(document.getElementsByClassName('jsNewElect')[0], 'click');
+                console.log("All comments elected.");
+                break;
+            default:
+                eventFire(document.getElementsByClassName('jsNewElect')[0], 'click');
+                window.setTimeout(Elect, 2000);
+        }
+    }
+
+    setInterval(() => { Elect(); }, 5000);
+
+})();
