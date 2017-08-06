a7f722632c87d7ecb22ebd8b341fb6ada7944f85
Crist Ye <iamCristYe@outlook.com>
2017-08-06 14:30:19 -0700

    Update Message2Article.js

--- Message2Article.js
+++ Message2Article.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Message2Article
 // @namespace    https://github.com/iamCristYe/MP-tools
-// @version      8.0
+// @version      9.0
 // @description  Automatically build article from messages.
 // @author       Crist
 // @match        https://mp.weixin.qq.com/cgi-bin/message?*
@@ -25,71 +25,73 @@
         window.location.href = "https://mp.weixin.qq.com/cgi-bin/message?t=message/list&count=1000&day=7&token=" + getParameterByName('token');
     }
 
-    var myInterval = setInterval(() => {
-        function GenerateHTML(DesiredMessageLength) {
-            var MessageArray = [].slice.call(document.getElementsByClassName("message_item"));
-            var resultArray = [];
-            for (var index = 0; index <= DesiredMessageLength; index++) {
-                var tmp = {};
+    unsafeWindow.GenerateHTML(DesiredMessageLength) {
+        var MessageArray = [].slice.call(document.getElementsByClassName("message_item"));
+        var resultArray = [];
+        for (var index = 0; index <= DesiredMessageLength; index++) {
+            var tmp = {};
 
-                //get UserID
-                if (MessageArray[index].getElementsByTagName("a").length == 9) {          //for images
-                    tmp.UserID = MessageArray[index].getElementsByTagName("a")[4].getAttribute("data-fakeid");
-                }
-                else {       //for texts and expired image
-                    tmp.UserID = MessageArray[index].getElementsByTagName("a")[2].getAttribute("data-fakeid");
-                }
-                //tmp.UserName = MessageArray[index].getElementsByTagName("a")[2].innerText;
-
-                tmp.TimeID = MessageArray[index].getAttribute("data-id");
-                tmp.Message = MessageArray[index].getElementsByClassName("wxMsg")[0].innerText;
-
-                //expired image
-                if (tmp.Message === "") tmp.Message = "(expired image)";
-
-                //get image URL
-                if (MessageArray[index].getElementsByClassName("appmsgSendedItem")[0] != null)
-                    tmp.pic = "https://mp.weixin.qq.com" + MessageArray[index].getElementsByClassName("appmsgSendedItem")[0].getElementsByTagName("a")[0].getAttribute("href");
-
-                resultArray.push(tmp);
+            //get UserID
+            if (MessageArray[index].getElementsByTagName("a").length == 9) {          //for images
+                tmp.UserID = MessageArray[index].getElementsByTagName("a")[4].getAttribute("data-fakeid");
             }
-            resultArray.sort((a, b) => {
-                if (a.UserID < b.UserID) {
-                    return -1;
-                }
-                if (a.UserID > b.UserID) {
-                    return 1;
-                }
-                if (a.TimeID < b.TimeID) {
-                    return -1;
-                }
-                if (a.TimeID > b.TimeID) {
-                    return 1;
-                }
-            });
-
-            var newHTML = ""; var lastUser = resultArray[0].UserID;
-            for (var index = 0; index < resultArray.length; index++) {
-                if (resultArray[index].UserID != lastUser) newHTML += "------<br>"; lastUser = resultArray[index].UserID;
-
-                if (resultArray[index].pic != null) newHTML += "<img src=" + resultArray[index].pic + "><br>";
-                else newHTML += resultArray[index].Message + "<br>";
-
+            else {       //for texts and expired image
+                tmp.UserID = MessageArray[index].getElementsByTagName("a")[2].getAttribute("data-fakeid");
             }
+            //tmp.UserName = MessageArray[index].getElementsByTagName("a")[2].innerText;
 
-            var win = window.open();
-            win.document.body.innerHTML = newHTML;
+            tmp.TimeID = MessageArray[index].getAttribute("data-id");
+            tmp.Message = MessageArray[index].getElementsByClassName("wxMsg")[0].innerText;
+
+            //expired image
+            if (tmp.Message === "") tmp.Message = "(expired image)";
+
+            //get image URL
+            if (MessageArray[index].getElementsByClassName("appmsgSendedItem")[0] != null)
+                tmp.pic = "https://mp.weixin.qq.com" + MessageArray[index].getElementsByClassName("appmsgSendedItem")[0].getElementsByTagName("a")[0].getAttribute("href");
+
+            resultArray.push(tmp);
+        }
+        resultArray.sort((a, b) => {
+            if (a.UserID < b.UserID) {
+                return -1;
+            }
+            if (a.UserID > b.UserID) {
+                return 1;
+            }
+            if (a.TimeID < b.TimeID) {
+                return -1;
+            }
+            if (a.TimeID > b.TimeID) {
+                return 1;
+            }
+        });
+
+        var newHTML = ""; var lastUser = resultArray[0].UserID;
+        for (var index = 0; index < resultArray.length; index++) {
+            if (resultArray[index].UserID != lastUser) newHTML += "------<br>"; lastUser = resultArray[index].UserID;
+
+            if (resultArray[index].pic != null) newHTML += "<img src=" + resultArray[index].pic + "><br>";
+            else newHTML += resultArray[index].Message + "<br>";
         }
 
+        var win = window.open();
+        win.document.body.innerHTML = newHTML;
+    }
+
+    function InsertPrompt() {
         clearInterval(myInterval);
 
         var MessageCount = [].slice.call(document.getElementsByClassName("message_item")).length;
 
         for (var index = 0; index < MessageCount; index++) {
             var div = document.createElement('div');
-            div.innerHTML = '<div style="text-align:center;cursor:pointer;" onClick="GenerateHTML(' + index + ')">汇总以上内容</div>';
+            div.setAttribute(style, "text-align:center;cursor:pointer;");
+            div.setAttribute(onClick, "GenerateHTML(" + index + ")");
+            div.innerHTML = '汇总以上内容';
             document.getElementsByClassName("message_item")[index].parentNode.insertBefore(div, document.getElementsByClassName("message_item")[index].nextSibling);
-
         }
-    }, 5000); //wait till all messages loaded
+    }
+
+    var myInterval = setInterval(() => { InsertPrompt(); }, 2000); //wait till all messages loaded
 })();
