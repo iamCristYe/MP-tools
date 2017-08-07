e208e8a7a165ff9ff11a99c6330242e4758223fd
Crist Ye <iamCristYe@outlook.com>
2017-08-07 13:42:30 -0700

    Update Message2Article.js

--- Message2Article.js
+++ Message2Article.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Message2Article
 // @namespace    https://github.com/iamCristYe/MP-tools
-// @version      13.0
+// @version      14.0
 // @description  Automatically build article from messages.
 // @author       Crist
 // @match        https://mp.weixin.qq.com/cgi-bin/message?*
